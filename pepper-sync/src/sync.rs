60b2a1a0b955e30074663cf2552a7b885e86ac5e
zancas <zancas@zingolabs.org>
2026-02-04 17:33:34 -0800

    start defining in house MockWallet for pepper-sync

--- pepper-sync/src/sync.rs
+++ pepper-sync/src/sync.rs
@@ -608,9 +608,10 @@ where
 }
 #[cfg(test)]
 mod test {
-
+    use crate::mocks;
     #[tokio::test]
     async fn my_first_test() {
+        //let test_wallet = create_test_wallet();
         assert_eq!(2, 2);
     }
 }
